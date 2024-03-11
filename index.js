5b3cca9e827c1f1dca672531270fe133cd091715
samuelnz2468 <samuel.christian.ong@gmail.com>
2024-03-11 18:38:49 +1300

    commitFile

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(1/0); // division by zero = Infinity
+console.log(-1/0); // negative division by zero = -Infinity
+console.log("not a number" / 1); // invalid mathematical operation = NaN
